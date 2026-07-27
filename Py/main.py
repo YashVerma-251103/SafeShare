44bbdf22990716366c293a425a8ed11b5c3be1ee
Pavy <yash23610@iiitd.ac.in>
2026-07-28 03:14:31 +0530

    formating driver files.

--- Py/main.py
+++ Py/main.py
@@ -8,46 +8,49 @@ import argparse
 
 
 def initialize():
-	ap = argparse.ArgumentParser(
-				description = __doc__,
-				formatter_class=argparse.RawDescriptionHelpFormatter
-		)
-	ap.add_argument("-p","--port",default=55001,type=int,help="Assign Server Port")
-	ap.add_argument("-sd","--shared_dir", type=Path, help="Absolute Path of the shared directory")
+    ap = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    ap.add_argument("-p", "--port", default=55001, type=int, help="Assign Server Port")
+    ap.add_argument(
+        "-sd", "--shared_dir", type=Path, help="Absolute Path of the shared directory"
+    )
 
-	args = ap.parse_args()
-	
-	port = args.port
+    args = ap.parse_args()
 
-	if args.shared_dir and Path.is_dir(args.shared_dir):
-		shared_dir = args.shared_dir
-	else:
-		print("Going with the Default Shared directory!")
-		default_shared_dir_name = "Shared"
-		shared_dir = Path(Path(__file__).parent.parent.resolve(),default_shared_dir_name)
-	
-	Path.mkdir(shared_dir,exist_ok=True)
+    port = args.port
 
-	return (shared_dir, port)
+    if args.shared_dir and Path.is_dir(args.shared_dir):
+        shared_dir = args.shared_dir
+    else:
+        print("Going with the Default Shared directory!")
+        default_shared_dir_name = "Shared"
+        shared_dir = Path(
+            Path(__file__).parent.parent.resolve(), default_shared_dir_name
+        )
 
+    Path.mkdir(shared_dir, exist_ok=True)
+
+    return (shared_dir, port)
 
 
 if __name__ == "__main__":
-	
-	shared_dir, port = initialize()
 
-	print(
-		"----------------------------------",
-		"SafeShare Node Starting...",
-		f"Sharing Folder: {shared_dir}",
-		"----------------------------------",
-		sep='\n',end=''
-	)
+    shared_dir, port = initialize()
 
-	Discovery.startAnnouncer("SafeShare-WebNode",port)
+    print(
+        "----------------------------------",
+        "SafeShare Node Starting...",
+        f"Sharing Folder: {shared_dir}",
+        "----------------------------------",
+        sep="\n",
+        end="",
+    )
 
-	server = Server(port,shared_dir)
-	server_thread = threading.Thread(target=server.run)
-	server_thread.start()
+    Discovery.startAnnouncer("SafeShare-WebNode", port)
 
-	MiniWeb.start_web_server()
+    server = Server(port, shared_dir)
+    server_thread = threading.Thread(target=server.run)
+    server_thread.start()
+
+    MiniWeb.start_web_server()
--- Py/temp.py
+++ Py/temp.py
@@ -1,22 +1,20 @@
 import argparse
 from pathlib import Path
+
 # print(Path(__file__))
 
 
-
 ap = argparse.ArgumentParser(
-            description = __doc__,
-            formatter_class=argparse.RawDescriptionHelpFormatter
-    )
-ap.add_argument("-p","--port",default=55001,type=int,help="Assign Server Port")
-ap.add_argument("-sd","--shared_dir", type=Path, help="Path of the shared directory")
+    description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+)
+ap.add_argument("-p", "--port", default=55001, type=int, help="Assign Server Port")
+ap.add_argument("-sd", "--shared_dir", type=Path, help="Path of the shared directory")
 
 args = ap.parse_args()
 
 port = args.port
 s = args.shared_dir
-print(port,s,args.shared_dir,type(s),type(args))
+print(port, s, args.shared_dir, type(s), type(args))
 
 
-
-default="shared"
+default = "shared"

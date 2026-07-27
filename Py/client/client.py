0da3174570ddf4ccb1e868cbb8b8d6257af663d4
Pavy <yash23610@iiitd.ac.in>
2026-07-28 03:05:55 +0530

    Client Complete

--- Py/client/client.py
+++ Py/client/client.py
@@ -1,116 +1,151 @@
 from typing import List
 import json, socket, struct
 
-from common.Enums import Status
-
-def __createRequestPayload(data:dict,format:str=">I"):# Big-Endian Unsigned 4-byte (32 bit) integer -- uint32_t
-    request = json.dump(data).encode()
-    payload_len = struct.pack(format, len(request))
-    send_payload = payload_len + request
-    return send_payload
-
-
-def __sendFrame(sock, data) -> int:
-    try:
-        send_payload = __createRequestPayload(data)
-        sock.sendall(send_payload)
-        return Status.SUCCESS
-    except OSError:
-        return Status.FAILED
-
-def __readFrame(sock,format:str=">I") -> int | dict:
-    try:
-        recv_payload_len = struct.unpack(format,sock.recv(4))[0]
-        response = json.load(sock.recv(recv_payload_len))
-        return response
-    except OSError:
-        return Status.FAILED
+from Py.common.protocol import Status, MSG
+from common.framing import *
 
 
 class Client:
     @staticmethod
-    def listRemoteFiles(ip:str,port:int,token:str,need_return:bool=False)->List[str] | None:
+    def listRemoteFiles(
+        ip: str, port: int, token: str, need_return: bool = False
+    ) -> List[str] | None:
         files = None
-    
-        with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0) as sock:
-            sock.connect((ip,port))
-    
-            send_data = {
-                "type" : "MSG_LIST",
-                "token" : token,
-                "path" : "."
-            }
-            status = __sendFrame(sock,send_data)
-            if (Status.FAILED == status):
-                print("Could Not send the payload. Aborting !")
-                
-            else:
-                response = __readFrame(sock)
-                if (Status.FAILED == response):
+        try:
+            with socket.socket(
+                family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0
+            ) as sock:
+                sock.connect((ip, port))
+
+                send_data = {"type": MSG.LIST_REQ, "token": token, "path": "."}
+                status = Frame.send(sock, send_data)
+                if Status.FAILED == status:
+                    print("Could Not send the payload. Aborting !")
+                    return None
+
+                response = Frame.read(sock)
+                if Status.FAILED == response:
                     print("Could not recieve the response. Aborting !")
-                    
-                else:
-                    if Status.RESPONSE_OK == response["status"]:
-                        if need_return:
-                            files = response["entries"]
-                        else:
-                            print("--- Remote Files ---")
-                            for file in response["entries"]: 
-                                print(f" - {file}")
+                    return None
+
+                header = response["header"]
+                if Status.RESPONSE_OK == header.get("status"):
+                    if need_return:
+                        files = header.get("entries", [])
                     else:
-                        print(f"Error: {response.get("message", "Unknown")}")
+                        print("--- Remote Files ---")
+                        for file in header.get("entries", []):
+                            print(f" - {file}")
+                else:
+                    print(f"Error: {response.get("message", "Unknown")}")
+
+        except OSError as e:
+            print(f"Connection Error : {e}")
 
         return files
 
-            
     @staticmethod
-    def sendPermRequest(ip:str,port:int,display_name:str,reason:str) -> str:
-        token = None
+    def sendPermRequest(ip: str, port: int, display_name: str, reason: str) -> str:
+        token = ""
 
-        with socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM,proto=0) as sock:
-            sock.connect((ip,port))
+        try:
+            with socket.socket(
+                family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0
+            ) as sock:
+                sock.connect((ip, port))
 
-            send_data = {
-                "type" : "MSG_PERM_REQUEST",
-                "display_name" : display_name,
-                "id" : display_name + "-req",
-                "from_device" : display_name + "-dev",
-                "reason" : reason
-            }
+                send_data = {
+                    "type": MSG.PERM_REQ,
+                    "display_name": display_name,
+                    "id": display_name + "-req",
+                    "from_device": display_name + "-dev",
+                    "reason": reason,
+                }
 
-            status = __sendFrame(sock,send_data)
-            if (Status.FAILED == status):
-                print("Could Not send the payload. Aborting !")
-        
-            else:
-                response = __readFrame(sock)
-                if (Status.FAILED == response):
+                status = Frame.send(sock, send_data)
+                if Status.FAILED == status:
+                    print("Could Not send the payload. Aborting !")
+                    return ""
+
+                response = Frame.read(sock)
+                if Status.FAILED == response:
                     print("Could not recieve the response. Aborting !")
+                    return ""
 
-                else:
-                    hdr,payload = __readFrame(sock)
-                    if (hdr["status"] == Status.ACCEPT):
-                        token = hdr["token"]
+                hdr = response["header"]
+                if Status.ACCEPT == hdr.get("status"):
+                    token = hdr.get("token", "")
+
+        except OSError as e:
+            print(f"Connection Error : {e}")
 
         return token
 
-
     @staticmethod
-    def downloadFile(ip:str,port:int,token:str,remote_path:str,local_path:str) -> str:
+    def downloadFile(
+        ip: str, port: int, token: str, remote_path: str, local_path: str
+    ) -> str:
         result = None
 
-        with socket.socket(family=socket.AF_INET,type=socket.SOCK_STREAM,proto=0) as sock:
-            sock.connect((ip,port))
+        try:
+            with socket.socket(
+                family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0
+            ) as sock:
+                sock.connect((ip, port))
 
-            send_data = {
-                "type": "MSG_DOWNLOAD_REQ",
-                "token":token,
-                "path" : remote_path
-            }
-            status = __sendFrame(sock,send_data)
-            if (Status.FAILED == status):
-                print("Could Not send the payload. Aborting !")
-            else:
-                pass
+                send_data = {
+                    "type": MSG.DOWNLOAD_REQ,
+                    "token": token,
+                    "path": remote_path,
+                }
 
-        return result
+                status = Frame.send(sock, send_data)
+                if Status.FAILED == status:
+                    print("Could Not send the payload. Aborting !")
+                    return "Connection Failed."
+
+                try:
+                    outfile = open(local_path, "wb")
+                except OSError:
+                    return f"File Write Error : {local_path}"
+
+                result = f"Download Complete : {local_path}"
+
+                try:
+                    while True:
+                        response = Frame.read(sock)
+                        if Status.FAILED == response:
+                            result = "Connection Lost."
+
+                            try:
+                                outfile.close()
+                            finally:
+                                if os.path.exists(local_path):
+                                    os.remove(local_path)
+                            break
+
+                        header = response["header"]
+                        payload = response["payload"]
+                        msg_type = header.get("type", "")
+
+                        if MSG.FILE_CHUNK == msg_type:
+                            if payload:
+                                outfile.write(payload)
+
+                        elif MSG.TRANSFER_END == msg_type:
+                            outfile.close()
+                            break
+
+                        elif MSG.ERROR == msg_type:
+                            result = f"Error : {header.get("message","Unknown")}"
+                            outfile.close()
+                            if os.path.exists(local_path):
+                                os.remove(local_path)
+                            break
+                finally:
+                    if not outfile.closed:
+                        outfile.close()
+
+                return result
+        except OSError as e:
+            return f"Connection Failed: {e}"
--- Py/client/main.py
+++ Py/client/main.py
@@ -2,33 +2,39 @@ import argparse, sys
 import json, socket, struct
 from typing import List
 
-from common.Enums import Commands,Status
+from Py.common.protocol import Commands, Status
 from discovery.udpListener import Listener
 from client.client import Client
 
 
-def showUsage()->None:
-    print("Usage : safeshare-client discover | request <ip> <port> <display_name> <reasons>")
+def showUsage() -> None:
+    print(
+        "Usage : safeshare-client discover | request <ip> <port> <display_name> <reasons>"
+    )
 
-def discoverPeers()->None:
-    peers = Listener.scanOnce(800)
+
+def discoverPeers(args: dict) -> None:
+    ms: int = args["ms"] if "ms" in args else 800
+    peers = Listener.scanOnce(ms)
     print(f"Found {len(peers)} peers:")
     for p in peers:
-        print(f"{p.name} ({p.device_id}) @ {p.ip} : {p.port}")  
-    
-def request(args:dict)->None:
+        print(f"{p.name} ({p.device_id}) @ {p.ip} : {p.port}")
 
-    if (len(args) != 5):
+
+def request(args: dict) -> None:
+
+    if len(args) != 5:
         print("Usage: safeshare-client request <ip> <port> <display_name> <reason>")
-        
+
     ip = args["ip"]
     port = args["port"]
     display = args["display"]
     reason = args["reason"]
     Client.sendPermRequest(ip, port, display, reason)
-    
-def listFiles(args:dict,need_return:bool=False)-> List[str] | None:
-    if (len(args) != 4):
+
+
+def listFiles(args: dict, need_return: bool = False) -> List[str] | None:
+    if len(args) != 4:
         print("Usage: safeshare-client list <ip> <port> <token>")
         return None
 
@@ -36,13 +42,16 @@ def listFiles(args:dict,need_return:bool=False)-> List[str] | None:
     port = args["port"]
     token = args["token"]
 
-    files = Client.listRemoteFiles(ip,port,token,need_return)
+    files = Client.listRemoteFiles(ip, port, token, need_return)
 
     return files
 
-def download(args:dict)->None:
+
+def download(args: dict) -> None:
     if len(args) != 6:
-        print("Usage: safeshare-client download <ip> <port> <token> <remote_path> <local_path>")
+        print(
+            "Usage: safeshare-client download <ip> <port> <token> <remote_path> <local_path>"
+        )
         return
 
     ip = args["ip"]
@@ -51,21 +60,50 @@ def download(args:dict)->None:
     r_path = args["r_path"]
     l_path = args["l_path"]
 
-    Client.downloadFile(ip,port,token,r_path,l_path)
+    Client.downloadFile(ip, port, token, r_path, l_path)
 
-def driver(args:dict)->None:
+
+def driver(args: dict) -> None:
     if "cmd" not in args:
         showUsage()
         return None
 
     cmd = args["cmd"]
 
-    if Commands.DISCOVER == cmd: discoverPeers()
-    elif Commands.REQUEST == cmd: request(args)
-    elif Commands.DOWNLOAD == cmd: download(args)
-    elif Commands.LIST == cmd: listFiles(args)
-    else: print("Reached else part")
+    if Commands.DISCOVER == cmd:
+        discoverPeers(args)
+    elif Commands.REQUEST == cmd:
+        request(args)
+    elif Commands.DOWNLOAD == cmd:
+        download(args)
+    elif Commands.LIST == cmd:
+        listFiles(args)
+    else:
+        print("Reached else part")
+
 
 if __name__ == "__main__":
-    pass
+    ap = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
 
+    all_args = {
+        "Cmd": ["--cmd", str, "Operation you want to perform."],
+        "Ip": ["--ip", str, "Machine IP you want to connect."],
+        "Port": ["--port", int, "Corresponding IP Port you want to connect to."],
+        "Display Name": ["--display", str, "Your Machine Display Name."],
+        "Reasons": ["--reason", str, "Reason for Connection with other Party."],
+        "Listen Time": ["--ms", int, "Time for listener scan in ms."],
+        "Auth Token": ["--token", str, "Auth Token recieved."],
+        "Remote Path": ["--r_path", str, "Remote Path"],
+        "Local Path": ["--l_path", str, "Local Path"],
+    }
+
+    for params in all_args.values():
+        ap.add_argument(params[0], type=params[1], help=params[2])
+
+    args = ap.parse_args()
+
+    args_dict = {k: v for k, v in vars(args).items() if v is not None}
+
+    driver(args=args_dict)
